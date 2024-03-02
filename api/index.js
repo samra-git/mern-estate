d3d3976123e3a4d937a78bbbb0d9223ab9aa03c7
Samra Harbi <samra.harbi78@gmail.com>
2024-03-02 19:14:22 +0100

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,17 @@
-import { log } from 'console';
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect("mongodb+srv://samra:samra@mern-estate.tqkf3kc.mongodb.net/?retryWrites=true&w=majority&appName=mern-estate").then(() => {
+    console.log('Connected to MongoDB!:)');
+}).catch((err) => {
+    console.log(err);
+});
+
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log('server is running on port 3000!!');
+    console.log('server is running on port 3000!');
 })
