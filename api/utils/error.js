6cd053ebf9fbb9a1b5f22f286d55ca67a7813eeb
Samra Harbi <samra.harbi78@gmail.com>
2024-03-06 17:09:16 +0100

    create sign in api route

--- api/utils/error.js
+++ api/utils/error.js
@@ -1,6 +1,6 @@
-export const errorHandler = (status, message) => {
+export const errorHandler = (statusCode, message) => {
     const error = new Error();
-    error.statusCode = status;
+    error.statusCode = statusCode;
     error.message = message;
     return error;
 }
